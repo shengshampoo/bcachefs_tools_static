bbe8473117694165fc2d2a5da920e091d1cd2422
shengshampoo <shengzh@tutanota.com>
2026-01-27 18:36:32 +0000

    Update build-static-bcachefs-tools.sh

--- build-static-bcachefs-tools.sh
+++ build-static-bcachefs-tools.sh
@@ -25,7 +25,7 @@ git clone https://github.com/koverstreet/bcachefs-tools.git
 cd bcachefs-tools
 sed -i "" -e '3s@PREFIX?=/usr/local$@PREFIX?=/usr/local/bcachefsmm@' ./Makefile
 sed -i "" -e '37s@std=gnu11@std=gnu23 -Wno-incompatible-function-pointer-types@' ./Makefile
-LDFLAGS="-static --static -no-pie -s" BCACHEFS_FUSE=0 make libbcachefs.a
+LDFLAGS="-static --static -no-pie -s" BCACHEFS_FUSE=1 make libbcachefs.a
 cd libbcachefs
 RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s" cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ../target/${HOST_ARCH}-chimera-linux-musl/release/
