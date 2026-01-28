2109d74da47d66ac11250d282582b414105d2b48
shengshampoo <shengzh@tutanota.com>
2026-01-28 17:56:46 +0000

    Update build-static-bcachefs-tools.sh

--- build-static-bcachefs-tools.sh
+++ build-static-bcachefs-tools.sh
@@ -13,8 +13,8 @@ HOST_ARCH=$(uname -m)
 cd $WORKSPACE
 curl https://git.kernel.org/pub/scm/linux/kernel/git/dhowells/keyutils.git/snapshot/keyutils-1.6.3.tar.gz | tar x --gzip
 cd keyutils-1.6.3
-#sed -i '' '/keyctl_restrict/d' ./version.lds
-#sed -i '' '/keyctl_dh_compute_kdf_alloc;/d' ./version.lds
+sed -i '' '/keyctl_restrict/d' ./version.lds
+sed -i '' '/keyctl_dh_compute_kdf_alloc;/d' ./version.lds
 sed -i "" -e 's@-Werror@-Wno-error@g' ./Makefile
 LDFLAGS="${LDFLAGS} -Wl,--undefined-version" make
 make LIBDIR=/usr/lib BINDIR=/usr/bin SBINDIR=/usr/sbin install
@@ -25,7 +25,7 @@ git clone https://github.com/koverstreet/bcachefs-tools.git
 cd bcachefs-tools
 sed -i "" -e '3s@PREFIX?=/usr/local$@PREFIX?=/usr/local/bcachefsmm@' ./Makefile
 sed -i "" -e '37s@std=gnu11@std=gnu23 -Wno-incompatible-function-pointer-types@' ./Makefile
-LDFLAGS="-static --static -no-pie -s" BCACHEFS_FUSE=1 make libbcachefs.a
+LDFLAGS="-static --static -no-pie -s" BCACHEFS_FUSE=0 make libbcachefs.a
 cd libbcachefs
 RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s" cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ../target/${HOST_ARCH}-chimera-linux-musl/release/
