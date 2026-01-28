bb1dd189b6be6f53188fdd7d9c69220617d66a9e
shengshampoo <shengzh@tutanota.com>
2026-01-28 18:16:27 +0000

    Update build-static-bcachefs-tools.sh

--- build-static-bcachefs-tools.sh
+++ build-static-bcachefs-tools.sh
@@ -19,26 +19,13 @@ sed -i "" -e 's@-Werror@-Wno-error@g' ./Makefile
 LDFLAGS="${LDFLAGS} -Wl,--undefined-version" make
 make LIBDIR=/usr/lib BINDIR=/usr/bin SBINDIR=/usr/sbin install
 
-#fuse
-cd $WORKSPACE
-curl -sL https://github.com/libfuse/libfuse/releases/download/fuse-3.18.1/fuse-3.18.1.tar.gz | tar x --gzip
-cd fuse-3.18.1
-#curl -sL https://gitlab.alpinelinux.org/alpine/aports/-/raw/master/main/fuse3/dont-mknod-dev-fuse.patch | patch -p1
-#curl -sL https://gitlab.alpinelinux.org/alpine/aports/-/raw/master/main/fuse3/mount_util.c-check-if-utab-exists-before-update.patch | patch -p1
-#curl -sL https://gitlab.alpinelinux.org/alpine/aports/-/raw/master/main/fuse3/workaround-the-lack-of-support-for-rename2-in-musl.patch | patch -p1
-mkdir build
-cd build
-meson setup --buildtype=release -Ddefault_library=static -Dprefix=/usr ..
-ninja
-ninja install
-
 # bcachefs-tools
 cd $WORKSPACE
 git clone https://github.com/koverstreet/bcachefs-tools.git
 cd bcachefs-tools
 sed -i "" -e '3s@PREFIX?=/usr/local$@PREFIX?=/usr/local/bcachefsmm@' ./Makefile
 sed -i "" -e '37s@std=gnu11@std=gnu23 -Wno-incompatible-function-pointer-types@' ./Makefile
-LDFLAGS="-static --static -no-pie -s" BCACHEFS_FUSE=1 make libbcachefs.a
+LDFLAGS="-static --static -no-pie -s" BCACHEFS_FUSE=0 make libbcachefs.a
 cd libbcachefs
 RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s" cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ../target/${HOST_ARCH}-chimera-linux-musl/release/
