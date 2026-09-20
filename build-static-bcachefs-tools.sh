c115556f0bb6dde8e4d4cc8540451c33f758d7d6
shengshampoo <shengzh@tutanota.com>
2026-09-20 01:50:01 +0000

    Update build-static-bcachefs-tools.sh

--- build-static-bcachefs-tools.sh
+++ build-static-bcachefs-tools.sh
@@ -19,6 +19,8 @@ sed -i "" -e 's@-Werror@-Wno-error@g' ./Makefile
 LDFLAGS="${LDFLAGS} -Wl,--undefined-version" make
 make LIBDIR=/usr/lib BINDIR=/usr/bin SBINDIR=/usr/sbin install
 
+cargo install bindgen-cli
+
 # bcachefs-tools
 cd $WORKSPACE
 git clone https://github.com/koverstreet/bcachefs-tools.git
